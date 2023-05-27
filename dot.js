abead2cfb0c1509cf1d1898a7657d2d16145e69a
Stratocaster <93025769+caster0987@users.noreply.github.com>
2023-05-27 11:53:50 +0530

    Update dot.js

--- dot.js
+++ dot.js
@@ -15,3 +15,7 @@ document.addEventListener("keydown", function (event) {
         event.preventDefault();
     }
 });
+
+function show(){
+    alert("A brother gifted with artistic might,  drawing colors, a world takes flight.   His voice, a melody, sweet and pure,   singing songs that forever endure. Through pages, he delves into tales. Reading, his spirit ever prevails. Amidst new faces that may appear,  he stands by me, unwaveringly near.  With his talents and loyalty true, my brother's love, forever in view.  United we stand, no matter the strife, bound by a bond, throughout our life.");
+}
