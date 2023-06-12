e7072cf7360137bb81c70dfa1de1186d28b66833
Stratocaster <93025769+caster0987@users.noreply.github.com>
2023-06-12 23:16:25 +0530

    Update dot.js

--- dot.js
+++ dot.js
@@ -16,23 +16,27 @@ document.addEventListener("keydown", function (event) {
     }
 });
 
-function show(){
-    alert("A brother gifted with artistic might,  drawing colors, a world takes flight.   His voice, a melody, sweet and pure,   singing songs that forever endure. Through pages, he delves into tales. Reading, his spirit ever prevails. Amidst new faces that may appear,  he stands by me, unwaveringly near.  With his talents and loyalty true, my brother's love, forever in view.  United we stand, no matter the strife, bound by a bond, throughout our life.");
-}
-
 const playButton = document.getElementById('playButton');
 const audio = document.getElementById('myAudio');
 
-// Add click event listener to the button
-playButton.addEventListener('click', function() {
-  // Check if the audio is paused
-  if (audio.paused) {
-    // Play the audio
-    audio.play();
-  } else {
-    // Pause the audio
-    audio.pause();
-  }
-});
+function playSongWithAlert() {
+  
+  audio.play();
+
+  setTimeout(function() {
+    
+    const message = "A brother gifted with artistic might, drawing colors, a world takes flight.\n" +
+    "His voice, a melody, sweet and pure, singing songs that forever endure.\n" +
+    "Through pages, he delves into tales. Reading, his spirit ever prevails.\n" +
+    "Amidst new faces that may appear, he stands by me, unwaveringly near.\n" +
+    "With his talents and loyalty true, my brother's love, forever in view.\n" +
+    "United we stand, no matter the strife, bound by a bond, throughout our life.";
+
+ 
 
 
+
+    alert(message)}, 100);
+}
+
+playButton.addEventListener('click', playSongWithAlert);
